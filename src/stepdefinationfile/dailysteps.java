d39b390ca8d5ae575327bbec492ad4be945efe2c
ladhanirahim <ladhani.rahim@yahoo.com>
2016-05-12 15:36:16 +1000

    some changes

--- src/stepdefinationfile/dailysteps.java
+++ src/stepdefinationfile/dailysteps.java
@@ -20,14 +20,14 @@ public class dailysteps {
 	@Before public void setup() 
 	{ driver = new FirefoxDriver(); }
 	
-	
+	//will navigate to website
 	@Given ("^i am on  ([^\"]*)$")
 	public void in(String str){
 		driver.navigate().to("http://gcloud-todos.appspot.com/examples/angularjs/#/");
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	
 	}
-	
+	//will delete all the todo items
 	@When ("i delete all todo")
 	
 	public void delete (String str){
@@ -38,7 +38,7 @@ public class dailysteps {
 		driver.findElement(By.id("toggle-all")).click();
 		driver.findElement(By.id("clear-completed")).click();
 		}
-
+//will search for any todo if left
 	@Then ("i dont see any todo in the list")
 	public void idDoesnotexist (String str){
 		List<WebElement> objElements1 = driver.findElements(By.id("filters"));
@@ -50,13 +50,15 @@ public class dailysteps {
 		}
 		}
 	}
-	
+	//will add a new todo to the list 
 	@When("i add a new ToDo")
 	
 	public void imeet (String str){
 		driver.findElement(By.id("new-todo")).sendKeys("firsttodo");
 		driver.findElement(By.id("new-todo")).sendKeys(Keys.ENTER);
 	}
+	
+	//will complete one todo and search if its deleted
 @When("i check the new todo as completed")
 	
 	public void acttod(String str1)
@@ -66,15 +68,9 @@ public class dailysteps {
 	driver.findElement(By.xpath("//*[@id='filters']/li[3]/a")).click();
 	driver.findElement(By.id("clear-completed")).click();
 	}
-List<WebElement> objElements1 = driver.findElements(By.xpath("//*[@id='todo-list']/li[2]/div/label"));
-{
-if (objElements1.size()>0){			
-	System.out.println(" - Obct not found");
-}else{			
-	System.out.println(" - Object not found");
+	
 }
-}}
-
+//will make sure todo is deleted
 @Then ("i dont see any todo to be completed")
 
 public void dontseeAnytodo(String str)
@@ -88,9 +84,9 @@ public void dontseeAnytodo(String str)
 	}
 	}}}
 	
-@Then ("i complete one todo")
-
-public void completeonetodo(String str)
+@Then ("click on active todo")
+//will show active todo
+public void activetodo(String str)
 {
 driver.findElement(By.xpath("//*[@id='filters']/li[2]/a")).click();
 List<WebElement> objElements1 = driver.findElements(By.xpath("//*[@id='todo-list']/li[3]/div/input"));
